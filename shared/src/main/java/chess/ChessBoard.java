09306984fece31a4dc5a3381a556403554519479
abwilson <abwilson2002@gmail.com>
2025-09-08 16:45:03 -0600

    Updated resetBoard to create a new game, also added HashCode and Equals

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,6 +1,10 @@
 package chess;
 
 import java.util.Dictionary;
+import java.util.Objects;
+
+import chess.ChessGame.TeamColor;
+import chess.ChessPiece.PieceType;
 
 /**
  * A chessboard that can hold and rearrange chess pieces.
@@ -56,10 +60,46 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        for (int i = 1; i < 9; i++) {
-            for (int j = 1; j < 9; j++) {
-                allPieces.remove(new ChessPosition(i, j));
+        if (!allPieces.isEmpty()) {
+            for (int i = 1; i < 9; i++) {
+                for (int j = 1; j < 9; j++) {
+                    if (allPieces.get(new ChessPosition(i,j)) != null) {
+                        allPieces.remove(new ChessPosition(i, j));
+                    }
+                }
             }
         }
+        ChessPiece placeHolder = new ChessPiece(TeamColor.WHITE, PieceType.PAWN);
+        int S = 1; //S stands for Side
+        int pawnLine = 2;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 1; j < 9; j++) {
+                allPieces.put(new ChessPosition(pawnLine, j),new ChessPiece(placeHolder.getTeamColor(), PieceType.PAWN));
+            }
+            allPieces.put(new ChessPosition(1,S), new ChessPiece(placeHolder.getTeamColor(), PieceType.ROOK));
+            allPieces.put(new ChessPosition(2,S), new ChessPiece(placeHolder.getTeamColor(), PieceType.KNIGHT));
+            allPieces.put(new ChessPosition(3,S), new ChessPiece(placeHolder.getTeamColor(), PieceType.BISHOP));
+            allPieces.put(new ChessPosition(4,S), new ChessPiece(placeHolder.getTeamColor(), PieceType.KING));
+            allPieces.put(new ChessPosition(5,S), new ChessPiece(placeHolder.getTeamColor(), PieceType.QUEEN));
+            allPieces.put(new ChessPosition(6,S), new ChessPiece(placeHolder.getTeamColor(), PieceType.BISHOP));
+            allPieces.put(new ChessPosition(7,S), new ChessPiece(placeHolder.getTeamColor(), PieceType.KNIGHT));
+            allPieces.put(new ChessPosition(8,S), new ChessPiece(placeHolder.getTeamColor(), PieceType.ROOK));
+            placeHolder = new ChessPiece(TeamColor.BLACK, PieceType.PAWN);
+            pawnLine = 7;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.equals(allPieces, that.allPieces);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(allPieces);
     }
 }
