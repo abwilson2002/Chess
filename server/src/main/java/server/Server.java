ff476f3361a2bf3a19f8782ddd5e413399eac878
abwilson <abwilson2002@gmail.com>
2025-10-09 09:09:14 -0600

    Implemented basic delete and register

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,24 +1,37 @@
 package server;
 
+import com.google.gson.Gson;
 import io.javalin.*;
+import io.javalin.http.Context;
+import java.util.Map;
 
 public class Server {
 
-    private final Javalin javalin;
+    private final Javalin javalinObj;
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        javalinObj = Javalin.create(config -> config.staticFiles.add("web"));
 
+        javalinObj.delete("db", ctx -> ctx.result("{}"));
+
+        javalinObj.post("user", ctx -> register(ctx));
         // Register your endpoints and exception handlers here.
 
     }
 
     public int run(int desiredPort) {
-        javalin.start(desiredPort);
-        return javalin.port();
+        javalinObj.start(desiredPort);
+        return javalinObj.port();
+    }
+
+    private void register(Context ctx) {
+        var serializer = new Gson();
+        var req = serializer.fromJson(ctx.body(), Map.class);
+        var res = Map.of("username", req.get("username"), "authToken", "xyz");
+        ctx.result(serializer.toJson(res));
     }
 
     public void stop() {
-        javalin.stop();
+        javalinObj.stop();
     }
 }
