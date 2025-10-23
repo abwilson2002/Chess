767063f74004f1abe75dcb32a9a82db34496d8cc
abwilson <abwilson2002@gmail.com>
2025-10-22 18:18:29 -0600

    Finished my tests

--- server/src/test/java/service/MyServiceTests.java
+++ server/src/test/java/service/MyServiceTests.java
@@ -2,6 +2,7 @@ package service;
 
 import chess.ChessGame;
 import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
 import dataaccess.MemoryDataAccess;
 import model.*;
 import org.junit.jupiter.api.*;
@@ -16,81 +17,225 @@ public class MyServiceTests {
     static UserData secondUser = new UserData("user2", "pass2", "2");
     static GameData firstGame = new GameData(1.0, null, null, "test", new ChessGame());
     static GameData secondGame = new GameData(2.0, null, null, "tester", new ChessGame());
+    static JoinData firstJoin = new JoinData(1.0, "WHITE", "hello");
     static DataAccess dataAccess = new MemoryDataAccess();
+    static UserService userService = new UserService(dataAccess);
 
 
     public static void init() {
-        dataAccess.addAuth(backgroundUser.username());
     }
 
-    @AfterAll
-    public static void close() {
+    @BeforeEach
+    public void start() throws DataAccessException {
+        var startingInfo = userService.register(backgroundUser);
+    }
+
+    @AfterEach
+    public void close() {
         dataAccess.clear();
     }
 
     @Test
     public void registerSuccess() {
-        dataAccess.addAuth(baseUser.username());
-        assert dataAccess.totalUsers() == 2;
+        try {
+            var result = userService.register(baseUser);
+            userService.register(secondUser);
+            assert dataAccess.totalUsers() == 3;
+            assert result.authToken() != null;
+            assert Objects.equals(result.username(), baseUser.username());
+        }
+        catch (Exception ex) {
+            assert false;
+        }
     }
 
     @Test
     public void registerFail() {
-
+        try {
+            var result = userService.register(backgroundUser);
+            assert dataAccess.totalUsers() == 2;
+        }
+        catch (Exception ex) {
+            assert dataAccess.totalUsers() == 1;
+        }
+        try {
+            userService.register(new UserData(null, "", null));
+            assert dataAccess.totalUsers() == 2;
+        }
+        catch (Exception ex) {
+            assert dataAccess.totalUsers() == 1;
+        }
     }
 
     @Test
     public void loginSuccess() {
+        try {
+            var result = userService.login(backgroundUser);
+            assert result.authToken() != null;
+            assert Objects.equals(result.username(), backgroundUser.username());
+            assert dataAccess.totalUsers() == 1;
+            assert dataAccess.totalAuths() == 2;
 
+            var secondResult = userService.register(baseUser);
+            assert !Objects.equals(secondResult.authToken(), result.authToken());
+            assert !Objects.equals(secondResult.username(), result.username());
+            assert dataAccess.totalUsers() == 2;
+            assert dataAccess.totalAuths() == 3;
+        }
+        catch (Exception ex) {
+            assert false;
+        }
     }
 
     @Test
     public void loginFail() {
-
+        try {
+            var result = userService.login(backgroundUser);
+            userService.login(backgroundUser);
+            assert result.authToken() != null;
+            assert Objects.equals(result.username(), backgroundUser.username());
+        }
+        catch (Exception ex) {
+            assert dataAccess.totalUsers() == 1;
+            assert dataAccess.totalAuths() == 2;
+        }
     }
 
     @Test
     public void logoutSuccess() {
+        try {
+            var userCodes = userService.login(backgroundUser);
+            var result = userService.logout(userCodes.authToken());
+            assert dataAccess.totalAuths() == 1;
+            assert dataAccess.totalUsers() == 1;
 
+        }
+        catch (Exception ex) {
+            assert false;
+        }
     }
 
     @Test
     public void logoutFail() {
-
+        try {
+            var userCodes = userService.login(backgroundUser);
+            userService.logout(userCodes.authToken());
+            userService.logout(userCodes.authToken());
+        }
+        catch (Exception ex) {
+            assert dataAccess.totalAuths() == 1;
+            assert dataAccess.totalUsers() == 1;
+        }
+        try {
+            var userCodes = userService.login(backgroundUser);
+            userService.logout(userCodes.username());
+        }
+        catch (Exception ex) {
+            assert dataAccess.totalAuths() == 2;
+            assert dataAccess.totalUsers() == 1;
+        }
+        try {
+            var userCodes = userService.login(backgroundUser);
+            userService.logout(baseUser.username());
+        }
+        catch (Exception ex) {
+            assert dataAccess.totalAuths() == 3;
+            assert dataAccess.totalUsers() == 1;
+        }
     }
 
     @Test
     public void listSuccess() {
-
+        try {
+            var userData = userService.login(backgroundUser);
+            var games = userService.list(userData.authToken());
+            assert dataAccess.totalGames() == 0;
+            assert games != null;
+        }
+        catch (Exception ex) {
+            assert false;
+        }
+        try {
+            var userData = userService.login(backgroundUser);
+            userService.create("hello", userData.authToken());
+            var games = userService.list(userData.authToken());
+            assert dataAccess.totalGames() == 1;
+        }
+        catch (Exception ex) {
+            assert false;
+        }
     }
 
     @Test
     public void listFail() {
-
+        try {
+            var userData = userService.login(backgroundUser);
+            userService.create("hello", userData.authToken());
+            var games = userService.list(userData.authToken());
+            userService.list(userData.username());
+        }
+        catch (Exception ex) {
+            assert dataAccess.totalGames() == 1;
+        }
     }
 
     @Test
     public void createSuccess() {
-
+        try {
+            var userData = userService.login(backgroundUser);
+            userService.create("hello", userData.authToken());
+            userService.create("hi there", userData.authToken());
+            assert dataAccess.totalGames() == 2;
+        }
+        catch (Exception ex) {
+            assert false;
+        }
     }
 
     @Test
     public void createFail() {
-
+        try {
+            var userData = userService.login(backgroundUser);
+            userService.create("hello", userData.authToken());
+            var games = userService.list(userData.authToken());
+            userService.list(userData.username());
+        }
+        catch (Exception ex) {
+            assert dataAccess.totalGames() == 1;
+        }
     }
 
     @Test
     public void joinSuccess() {
-
+        try {
+            var userData = userService.login(backgroundUser);
+            userService.create("hello", userData.authToken());
+            var tester = userService.join(firstJoin, userData.authToken());
+            assert true;
+        }
+        catch (Exception ex) {
+            assert false;
+        }
     }
 
     @Test
     public void joinFail() {
-
+        try {
+            var userData = userService.login(backgroundUser);
+            userService.create("hello", userData.authToken());
+            var tester = userService.join(firstJoin, userData.authToken());
+            userService.join(firstJoin, userData.authToken());
+        }
+        catch (Exception ex) {
+            assert true;
+        }
     }
 
     @Test
     public void clearSuccess() {
-
+        dataAccess.clear();
+        assert dataAccess.totalUsers() == 0;
+        assert dataAccess.totalAuths() == 0;
+        assert dataAccess.totalGames() == 0;
     }
 }
